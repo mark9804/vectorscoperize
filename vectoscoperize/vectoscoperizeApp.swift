686c610e4fd99869a252772c877cb8307687615a
Mark Chen <9006264+mark9804@users.noreply.github.com>
2025-12-13 02:32:48 +0900

    Initial Commit

---
+++ vectoscoperize/vectoscoperizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  vectoscoperizeApp.swift
+//  vectoscoperize
+//
+//  Created by Zhaolu Chen on 2025/12/13.
+//
+
+import SwiftUI
+
+@main
+struct vectoscoperizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
